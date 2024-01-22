76ad7983252d8aac3ec7469bb39837453791d45a
Anthony Swierkosz <anthony@swierkosz.dev>
2024-01-21 21:56:31 -0500

    Hopefully fix explorer script

--- scripts/Explorer.ps1
+++ scripts/Explorer.ps1
@@ -1,9 +1,13 @@
+# Start
+New-Item -Path C:\ -Name "Start.txt" -ItemType File -Value $(Get-Date)
 # Set Windows Explorer display options
 New-Item -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Force | Out-Null
 Set-ItemProperty -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ -Name "HideFileExt" -Value 0 # Show file extensions
 Set-ItemProperty -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ -Name "HideDrivesWithNoMedia" -Value 0 # Show empty drives
 Set-ItemProperty -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ -Name "Hidden" -Value 1 # Show hidden files
 Set-ItemProperty -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ -Name "LaunchTo" -Value 1 # Open File Explorer to This PC
-
+# Top Level
 Set-ItemProperty -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\ -Name "ShowRecent" -Value 0 # Remove recent files
 Set-ItemProperty -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\ -Name "ShowFrequemt" -Value 0 # Remove frequent folders
+# End
+New-Item -Path C:\ -Name "End.txt" -ItemType File -Value $(Get-Date)
